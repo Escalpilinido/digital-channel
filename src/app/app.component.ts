6eefa40f447a4dcb50632d8284f71197e97402ad
Jorge Jesús Campoy López <jorgecampoy81@gmail.com>
2026-01-29 23:45:30 +0100

    feat: preparing table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { usersData } from '../assets/users.mock';
+
+
+interface UserData {
+  name: string;
+  surname: string;
+  seniority: string;
+  years: number;
+  availability: boolean;
+  descripcion: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatTableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'digital_chanel_front';
+  displayedColumns: string[] = ['name', 'surname', 'seniority', 'years', 'availability', 'descripcion'];
+  dataSource: UserData[] = usersData;
 }
