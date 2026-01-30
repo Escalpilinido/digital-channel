6a7a0fed4d9a64f517685022bc2d83fd505eb294
Jorge Jesús Campoy López <jorgecampoy81@gmail.com>
2026-01-30 01:23:23 +0100

    feat: added toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,15 @@
 import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { usersData } from '../assets/users.mock';
 import * as XLSX from 'xlsx';
-
-export interface UserData {
-  name: string;
-  surname: string;
-  seniority: string;
-  years: number;
-  availability: boolean;
-  descripcion: string;
-}
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatTableModule, MatButtonModule, MatPaginatorModule, RouterModule],
+  imports: [RouterModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit {
-  displayedColumns: string[] = ['name', 'surname', 'seniority', 'years', 'availability'];
-  dataSource = new MatTableDataSource<UserData>(usersData);
+export class AppComponent {
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
-
-  exportToExcel() {
-    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.dataSource.data);
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Usuarios');
-
-    /* guardar el archivo */
-    XLSX.writeFile(wb, 'Usuarios_DigitalChanel.xlsx');
-  }
 }
