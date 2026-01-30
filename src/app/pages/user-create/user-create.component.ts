533bbd98b2fdddfbf87b99d265a047a3639a76a2
Jorge Jesús Campoy López <jorgecampoy81@gmail.com>
2026-01-30 16:49:25 +0100

    feat: almost finished

--- src/app/pages/user-create/user-create.component.ts
+++ src/app/pages/user-create/user-create.component.ts
@@ -3,7 +3,9 @@ import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
-import { RouterLink, RouterModule } from "@angular/router";
+import { Router, RouterModule } from "@angular/router";
+import { UsersService } from "../../services/users.service";
+import { UserData } from "../../../assets/users.mock";
 
 @Component({
     selector: 'app-user-create',
@@ -14,6 +16,8 @@ import { RouterLink, RouterModule } from "@angular/router";
 })
 export class UserCreateComponent {
     private fb = inject(FormBuilder)
+    private usersService = inject(UsersService)
+    private router = inject(Router)
 
     formNewUser = this.fb.group({
         name: ['', [Validators.required]],
@@ -25,8 +29,8 @@ export class UserCreateComponent {
     })
 
     saveUser() {
-        if (this.formNewUser.valid) {
-            console.log(this.formNewUser.value);
-        }
+        const newUserData = this.formNewUser.getRawValue() as unknown as UserData;
+        this.usersService.createUser(newUserData);
+        this.router.navigate(['/user-info']);
     }
 }
--- src/app/resolvers/users.resolver.ts
+++ src/app/resolvers/users.resolver.ts
@@ -1,14 +1,13 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
-import { UsersService } from '../services/users';
-import { UserData } from '../../assets/users.mock';
-import { Observable } from 'rxjs';
+import { UsersService } from '../services/users.service';
 
 /**
  * Resolver for user-info route
+ * We will simulate init the app and users data and load in a behavior subject
  */
-export const usersResolver: ResolveFn<Observable<UserData[]>> = () => {
+export const usersResolver: ResolveFn<void> = () => {
     const usersService = inject(UsersService);
 
-    return usersService.initUsers();
+    usersService.initUsers();
 };
